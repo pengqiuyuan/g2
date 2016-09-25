7f79299cb209a4270843aabde4239589fff977e1
pengqiuyuanfj@gmail.com <pengqiuyuanfj@gmail.com>
2016-09-25 12:46:25 +0800

    bug
    bug

--- server/src/main/java/com/g2/web/account/LoginController.java
+++ server/src/main/java/com/g2/web/account/LoginController.java
@@ -41,6 +41,7 @@ public class LoginController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String fail(@RequestParam(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM) String userName, Model model) {
 		model.addAttribute(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM, userName);
+		SecurityUtils.getSubject().logout();
 		return "account/login";
 	}
 
